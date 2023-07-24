5832a3a56e0b8c7a6b95d465e9883ccba0411017
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2023-07-24 20:12:51 -0300

    small-modify: file title.php renamed to name.php

---
+++ src/domain/validators/nome.php
@@ -0,0 +1,40 @@
+<?php
+
+class Name {
+  private string $name;
+
+  private function __construct(string $name) {
+    $this->name = $name;
+  }
+
+  public static function create (string $name): Name {
+    $name = trim($name);
+    $name = preg_replace('/( )+/', ' ', $name);
+
+    if (!Name::validate($name)) {
+    }
+
+    return new Name($name);
+  }
+
+  public function getValue(): string {
+    return $this->name;
+  }
+
+  private static function validate(string $name): bool {
+    if (
+      !$name ||
+      strlen($name) < 10 ||
+      strlen($name) > 255
+    ) {
+      return false;
+    }
+    
+    return true;
+  }
+}
+
+$name = Name::create('Titulo da     minha   tarefa!');
+
+print_r($name);
+print_r($name->getValue());
