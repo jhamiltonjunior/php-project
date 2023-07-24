09234c4b667901c0a0a1f3d25804775467b26f00
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2023-07-23 22:13:41 -0300

    first commit

---
+++ requeriment.md
@@ -0,0 +1,8 @@
+# O que eu preciso para criar um pomodoro/lista de tarefas
+
+- titulo
+- temporizador
+- descricao
+- pause (opcional)
+- continue (opcional)
+- parar (obrigatório)
---
+++ src/domain/validators/title.php
@@ -0,0 +1,39 @@
+<?php
+
+class Title {
+  private string $title;
+
+  public function __construct(string $title) {
+    $this->title = $title;
+  }
+
+  public static function create (string $title): Title {
+    $title = trim($title);
+    $title = str_replace('  ', ' ', $title);
+
+    if (!Title::validate($title)) {
+    }
+
+    return new Title($title);
+  }
+
+  public function getValue(): string {
+    return $this->title;
+  }
+
+  private static function validate(string $title): bool {
+    if (
+      !$title ||
+      strlen($title) < 10 ||
+      strlen($title) > 255
+    ) {
+      return false;
+    }
+    
+    return true;
+  }
+}
+
+$title = Title::create('Titulo da minha tarefa!');
+
+echo $title;
